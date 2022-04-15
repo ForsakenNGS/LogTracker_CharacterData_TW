157fbd5cd7ab93c98d5811f39c0651e5a8320785
Jens N <jens.n@godlike.biz>
2022-04-15 06:52:13 +0200

    Addon data update 2022-04-15 06:52

--- LogTracker_CharacterData_TW.lua
+++ LogTracker_CharacterData_TW.lua
@@ -1,1 +1,100 @@
-LogTracker_CharacterData_TW = {};
+LogTracker_CharacterData_TW = {
+  [ "Souporman-伊弗斯" ] = {
+    level = "0",
+    faction = "Horde",
+    guild = nil,
+    last_update = "2022-04-15 06:51:11",
+    last_update_int = 1649998271,
+    performance = {
+      {
+        zoneId = 1011,
+        zoneName = "BT / Hyjal",
+        partition = 5,
+        partitionName = "P3.5 & P4",
+        allstars = {
+          {
+            spec = "Shadow",
+            percentRank = 45.245395719263,
+          },
+          {
+            spec = "Holy",
+            percentRank = 30.973624574351,
+          },
+        },
+        encountersKilled = 14,
+      },
+      {
+        zoneId = 1012,
+        zoneName = "Zul'Aman",
+        partition = 5,
+        partitionName = "P3.5 & P4",
+        allstars = {
+          {
+            spec = "Shadow",
+            percentRank = 25.713865175155,
+          },
+        },
+        encountersKilled = 6,
+      },
+    },
+  },
+  [ "天野銀次-伊弗斯" ] = {
+    level = "0",
+    faction = "Horde",
+    guild = nil,
+    last_update = "2022-04-15 06:51:27",
+    last_update_int = 1649998287,
+    performance = {
+      {
+        zoneId = 1011,
+        zoneName = "BT / Hyjal",
+        partition = 5,
+        partitionName = "P3.5 & P4",
+        allstars = {},
+        encountersKilled = 14,
+      },
+      {
+        zoneId = 1012,
+        zoneName = "Zul'Aman",
+        partition = 5,
+        partitionName = "P3.5 & P4",
+        allstars = {},
+        encountersKilled = 6,
+      },
+    },
+  },
+  [ "小蔦冰涼-伊弗斯" ] = {
+    level = "0",
+    faction = "Horde",
+    guild = nil,
+    last_update = "2022-04-15 06:47:47",
+    last_update_int = 1649998067,
+    performance = {
+      {
+        zoneId = 1011,
+        zoneName = "BT / Hyjal",
+        partition = 5,
+        partitionName = "P3.5 & P4",
+        allstars = {
+          {
+            spec = "Destruction",
+            percentRank = 81.650856430698,
+          },
+          {
+            spec = "Affliction",
+            percentRank = 4.5414152752444,
+          },
+        },
+        encountersKilled = 14,
+      },
+      {
+        zoneId = 1012,
+        zoneName = "Zul'Aman",
+        partition = 5,
+        partitionName = "P3.5 & P4",
+        allstars = {},
+        encountersKilled = 6,
+      },
+    },
+  },
+};
